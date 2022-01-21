78fd8a0153d57400022ef8dfdfc95d6a70679a44
Stanislav Pankevich <s.pankevich@gmail.com>
2022-01-21 22:55:56 +0100

    WIP

---
+++ strictdoc/cli/config_parser.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+# from tomlkit import parse
+
+
+class SDocConfig:
+    def __init__(self):
+        pass
+
+
+class ConfigParser:
+    @staticmethod
+    def parse_config() -> Optional[SDocConfig]:
+        # with open("strictdoc.toml", "r") as config_file:
+        #     config_content = config_file.read()
+        # config_dict = parse(config_content)
+        return None
